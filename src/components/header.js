553fe8d5c9cc506afa77db04c35fd45b366edb54
Taylor Page <taylor@thepagesmedia.com>
2019-06-08 05:37:42 -0500

    added some starter info

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#2D5B2D`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>We are WC Partners</h1>
+    <p>Welcome to the new website for WC Partners</p>
+    <p>The mission of Webster County is to advocate, enhance services, and positively impact the lives of citizens with developmental disabilities.
+</p>
+    <div style={{ maxWidth: `350px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
