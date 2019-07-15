466d7550ec0a2b1bb9672b1b116d7cffcd00a3f0
Taylor Page <taylor@thepagesmedia.com>
2019-07-14 19:58:59 -0500

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,21 @@ module.exports = {
         icon: `src/images/wclogo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-143787607-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "www.wc-partners.net",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
