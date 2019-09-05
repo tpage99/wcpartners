8f9bfe30b032d023ebea5394ac099fe9cd012784
Taylor <taylor@thepagesmedia.com>
2019-09-05 09:37:34 -0500

    updated activity list and update date

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -7,7 +7,7 @@ import HeroHeader from "../components/Navbar/heroHeader"
 import QuickContact from "../components/quick-contact"
 import Footer from "../components/footer"
 
-import pdf from "./resources/August2019.pdf"
+import pdf from "./resources/September2019.pdf"
 
 const Resources = ({ data }) => (
   <Fragment>
@@ -24,7 +24,7 @@ const Resources = ({ data }) => (
           </h2>
         </button>
         <p style={{ marginTop: `0`, paddingTop: `0`, fontSize: `0.9rem` }}>
-          <em>Click to download. Last updated July 31, 2019</em>
+          <em>Click to download. Last updated September 5, 2019</em>
         </p>
         <p>
           The activity list is created by Support Coordinators to include
